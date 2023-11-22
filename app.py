d44ea5054478b0a8da42d964d220d4e7f0040b58
gerardcolomerFV <gerard@freeverse.io>
2023-11-22 19:31:46 +0100

    mpv completed using ipfs.io pending tokenURI fixes

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify
 import re
 import json
-
+from web3 import Web3
+import requests
 
 app = Flask(__name__)
 
@@ -46,8 +47,9 @@ def get_ul_fields(path):
     }
     return(global_consensus, parachain, account_key, general_key)
 
+
 # Define a function to get the RPC URL from the config given the global consensus and parachain
-def get_chain_rpc(global_consensus, parachain=""):
+def get_chain_info(global_consensus, parachain=""):
     """
     Get the RPC URL from the configuration based on the global consensus and parachain.
 
@@ -58,25 +60,75 @@ def get_chain_rpc(global_consensus, parachain=""):
     config = load_config()  # Load the configuration
     consensus_config = config.get('GlobalConsensusMappings', {}).get(global_consensus, {})
     parachain_config = consensus_config.get('Parachains', {}).get(parachain, {})
-    return parachain_config.get('rpc')  # Return the RPC URL or None if not found
+    return parachain_config.get('rpc'), parachain_config.get('chainId')  # Return the RPC URL or None if not found
+
+# Function to get the tokenURI from a smart contract
+def get_token_uri(rpc_url, contract_address, asset_id):
+    """
+    Call an EVM chain RPC node to get the tokenURI for a given contract address and assetId.
+
+    :param rpc_url: The RPC URL of the EVM-compatible blockchain node.
+    :param contract_address: The address of the smart contract.
+    :param asset_id: The asset ID for which to retrieve the token URI.
+    :return: The token URI if found, otherwise None.
+    """
+    # Initialize a web3 connection to the RPC
+    web3 = Web3(Web3.HTTPProvider(rpc_url))
+
+    # Define the contract ABI to include only the function we're interested in (tokenURI)
+    contract_abi = [
+        {
+            "constant": True,
+            "inputs": [{"name": "_tokenId", "type": "uint256"}],
+            "name": "tokenURI",
+            "outputs": [{"name": "", "type": "string"}],
+            "payable": False,
+            "stateMutability": "view",
+            "type": "function",
+        },
+    ]
+
+    # Create a contract object using the contract address and ABI
+    contract = web3.eth.contract(address=contract_address, abi=contract_abi)
+
+    try:
+        # Call the tokenURI function of the contract
+        token_uri = contract.functions.tokenURI(int(asset_id)).call()
+        return token_uri
+    except Exception as e:
+        app.logger.error(f"An error occurred when fetching token URI: {e}")
+        return None
+
+def fetch_ipfs_data(token_uri):
+    #TODO Modify function in order to filder for ipfs:// when the tokenURI returns it
+    # Check if the token URI already includes the IPFS gateway URL
+    if not token_uri.startswith('https://ipfs.io/ipfs/'):
+        token_uri = 'https://ipfs.io/ipfs/' + token_uri
+
+    try:
+        response = requests.get(token_uri)
+        response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
+        return response.json()
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")  # Python 3.6
+        return None
+    except Exception as err:
+        print(f"An error occurred: {err}")  # Python 3.6
+        return None
 
 @app.route('/<path:path>', methods=['GET'])
 def handle_request(path):
     """
-    Handle the incoming request and parse the URL for specific components.
+    Handle the incoming request and parse the URL to get the content of the tokenURI of that Universal Location.
 
     :param path: The full path from the request URL.
     :return: A JSON response with the parsed components.
     """
     global_consensus, parachain, account_key, general_key = get_ul_fields(path)
-    rpc_url = get_chain_rpc(global_consensus, parachain)
-    return jsonify({
-        "GlobalConsensus": global_consensus,
-        "Parachain": parachain,
-        "AccountKey20": account_key,
-        "GeneralKey": general_key,
-        "rpc_url":rpc_url
-    })
+    rpcUrl, chainId = get_chain_info(global_consensus, parachain)
+    tokenUri = get_token_uri(rpcUrl, account_key, general_key)
+    tokenURIResult = fetch_ipfs_data(tokenUri)
+    return jsonify(tokenURIResult)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
